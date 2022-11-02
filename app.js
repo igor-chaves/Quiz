a86f377e7a47920e3e5f2acfbf45b4ff49377345
Igor <ichaves@live.com>
2022-11-02 17:45:07 +0900

    New quiz
    Re-building the quiz from scratch with a new layout

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-const form = document.querySelector(".quiz-form")
-const correctAnswers = ["B", "B", "B", "B"]
+const form = document.querySelector("form")
+
 
 form.addEventListener("submit", e => {
     e.preventDefault()
 
-    let score = 0
-    const userAnswers = [
-        form.inputQuestion1.value,
-        form.inputQuestion2.value,
-        form.inputQuestion3.value,
-        form.inputQuestion4.value
-    ]
-
-    userAnswers.forEach((userAnswer, index) => {
-        if (userAnswer === correctAnswers[index]) {
-            score += 25
-            console.log("Right answer!!")
-        }
-    })
-    console.log(score);
+    
 })
+
+console.log(form.question1)
